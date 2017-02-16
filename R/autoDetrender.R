c0222f95c8526b231efe592f813485312b5f6f6e
Fosco <fosco.vesely@gmail.com>
2017-02-16 15:40:50 +0100

    code included

---
+++ R/autoDetrender.R
@@ -0,0 +1,107 @@
+library(trend)
+
+virgilio<-function(){
+	if(any(names(yieldPrev)== "actualYield")){}else{
+	suppressWarnings(configure())}
+	suppressWarnings(checkTrends())
+	if(adf.test(yieldPrev$flatYield$OFFICIAL_YIELD)$p.value > 0.25) {suppressWarnings(try(autoProposal(),silent=T))}
+	while(yieldPrev$flattyn == "y"){
+		suppressWarnings(breakTrends())
+		suppressWarnings(checkTrends())
+	}
+	suppressWarnings(modSel())
+	suppressWarnings(responseYield())
+}
+
+
+
+autoProposal<-function(){
+	if(yieldPrev$flattyn =="y"){
+		#while(adf.test(yieldPrev$flatYield$OFFICIAL_YIELD)$p.value > 0.25){
+			suppressMessages(suppressWarnings(autoDetrender()))
+			#}
+		dev.new()
+		attach(yieldPrev)
+			#offiPlot<-ggplot(flatYield,aes(x=YEAR, y=OFFICIAL_YIELD,group=1))+geom_point(color="black")+geom_line(color="black")+geom_smooth(method="lm",color="brown",se=FALSE)+geom_smooth(method="loess",color="red",se=FALSE)+geom_smooth(method="lm",formula= y~splines::bs(x,3),color="orange",se=FALSE)
+			#plot(offiplot)
+
+		flatPlot<-ggplot(yieldPrev$flatYield)+geom_point(color="black",aes(x=YEAR, y=OFFICIAL_YIELD,group=1))+geom_line(color="black",aes(x=YEAR, y=OFFICIAL_YIELD,group=1))+geom_smooth(method="lm",color="brown",se=FALSE,aes(x=YEAR, y=OFFICIAL_YIELD,group=1))+geom_smooth(method="loess",color="red",se=FALSE,aes(x=YEAR, y=OFFICIAL_YIELD,group=1))+geom_smooth(method="lm",formula= y~splines::bs(x,3),color="orange",se=FALSE,aes(x=YEAR, y=OFFICIAL_YIELD,group=1))
+		flatPlot<-flatPlot + geom_rect(data=yieldPrev$breakPoint, aes(xmin=begin, xmax=finish, ymin=-Inf, ymax=+Inf), fill='yellow', alpha=0.3)
+		plot(flatPlot)
+
+		detach(yieldPrev)
+		cat("This is the result of an automated de trending procedure. \n Do you want to use it? \n y : save it as a starting point \n n : drop it and remove trend from original data \n ")
+		appAuto<-scan(,what="text",nmax=1)
+		while(appAuto != "y" & appAuto != "n"){
+			cat("answer y or n \n ")
+			appAuto<-scan(,what="text",nmax=1)
+		}
+		if(appAuto == "n"){
+			rm(list=c("breakPoint","flatYield","due2trend","friendShip","flattyn","safeTrend","yieldTrend","flatOff","tableXregression","model_formula","CVmsRes","expYield","omniYield","modelLM","PCmodel"),envir=yieldPrev)
+			yieldPrev$flatYield<-yieldPrev$actualYield
+		}
+		dev.off()
+
+	}
+}
+
+autoDetrender<-function(){
+	#acquire te data and clean'em
+	tableXregression <-merge(yieldPrev$flatYield , yieldPrev$relatedModel , by="YEAR")
+	#clean this table, 0 columns are going to mess it up
+	coluClean<-function(cola){
+		if(min(tableXregression[,cola]) == max(tableXregression[,cola])){return(cola)}
+	}
+	dirtyCol<-lapply(X=seq(1,length(names(tableXregression))),FUN=coluClean)
+	if(!is.null(unlist(dirtyCol))){tableXregression<-tableXregression[,-unlist(dirtyCol)]}
+	#check existance of break point
+	if(pettitt.test(tableXregression$OFFICIAL_YIELD)$p.value <= 0.5 & pettitt.test(tableXregression$OFFICIAL_YIELD)$estimate > 1 ){
+		untrendingIt(tableXregression$YEAR[pettitt.test(tableXregression$OFFICIAL_YIELD)$estimate],tableXregression)
+	}
+	while(yieldPrev$refeedAutoTrend < yieldPrev$currentYear){
+
+		try(untrendingIt(yieldPrev$refeedAutoTrend,tableXregression))}
+
+}
+
+untrendingIt<-function(inizio,tableXregression){
+	#find out pre-trend data
+	tableXpast<-subset(tableXregression,tableXregression$YEAR<=inizio)
+	#check pre-trend dynamics
+			#resarima<-auto.arima(tableXpast$OFFICIAL_YIELD,xreg=tableXpast[,-c(which(names(tableXpast) == "YEAR"),which(names(tableXpast) == "OFFICIAL_YIELD"))],seasonal=FALSE,allowdrift=F,allowmean=T)
+			model_full <- lm(OFFICIAL_YIELD ~ ., data = tableXpast[,-c(which(names(tableXpast) == "YEAR"))])
+			scopeformula <- formula(model_full)
+			resarima <- step(object=model_full, scope=scopeformula, direction="both", trace = 0) #<== larger K reduce the suit to involve more predictors
+		#trended data
+	tableXfuture<-subset(tableXregression,tableXregression$YEAR>inizio)
+	#check if trend ended
+	if(pettitt.test(tableXfuture$OFFICIAL_YIELD)$p.value <= 0.5 & pettitt.test(tableXfuture$OFFICIAL_YIELD)$estimate != length(tableXfuture$OFFICIAL_YIELD& pettitt.test(tableXfuture$OFFICIAL_YIELD)$estimate > 1)){
+		fine<-tableXfuture$YEAR[pettitt.test(tableXfuture$OFFICIAL_YIELD)$estimate]
+		print(fine)
+		tableXfuture<-subset(tableXfuture,tableXfuture$YEAR <= fine)
+		}else{fine<-yieldPrev$currentYear}
+			#futarima<-predict(resarima,newxreg=tableXfuture[,-c(which(names(tableXfuture) == "YEAR"),which(names(tableXfuture) == "OFFICIAL_YIELD"))])
+			futarima<-predict(resarima,newdata=tableXfuture[,-c(which(names(tableXfuture) == "YEAR"),which(names(tableXfuture) == "OFFICIAL_YIELD"))],se.fit=TRUE,type="response",level=0.95,interval="prediction")
+			#yieldPrev$safeTrend<-lm(futarima$pred~seq(1,length(futarima$pred)))$coefficients[2]
+			yieldPrev$safeTrend<-lm(futarima$fit[,1]~seq(1,length(futarima$fit[,1])))$coefficients[2]
+	cutTrend(inizio,fine)
+	yieldPrev$refeedAutoTrend<-fine
+
+}
+
+
+
+#					tableXregression <-merge(yieldPrev$flatYield , yieldPrev$relatedModel , by="YEAR")
+#					#clean this table, 0 columns are going to mess it up
+#					coluClean<-function(cola){
+#						if(min(tableXregression[,cola]) == max(tableXregression[,cola])){return(cola)}
+#					}
+#					dirtyCol<-lapply(X=seq(1,length(names(tableXregression))),FUN=coluClean)
+#					if(!is.null(unlist(dirtyCol))){tableXregression<-tableXregression[,-unlist(dirtyCol)]}
+#					#check existance of break point
+#					if(pettitt.test(tableXregression$OFFICIAL_YIELD)$p.value <= 0.5){
+
+#						offiPlot<-ggplot(flatYield,aes(x=YEAR, y=OFFICIAL_YIELD,group=1))+geom_point(color="black")+geom_line(color="black")+geom_smooth(method="lm",color="brown",se=FALSE)+geom_smooth(method="loess",color="red",se=FALSE)+geom_smooth(method="lm",formula= y~splines::bs(x,3),color="orange",se=FALSE)
+#						plot(offiPlot)
+#						autoDetrender()
+#					}
---
+++ R/batchYield.R
@@ -0,0 +1,70 @@
+#!/usr/bin/R
+#extension for batch aplication of foreYield
+
+
+#data importing front-end function (structure)
+#allow for override with a saved detrended official dataset
+importYield<-function(offiData,simuData,allData,crp,cnt,dec){
+	if(missing(allData)){
+		write.table(gsub(gsub(scan(file=offiData,what="text",sep="\n"),pattern='"',replacement='',fixed=TRUE),pattern=";",replacement=","),file="adjOffi.csv",quote=FALSE,row.names=FALSE,col.names=FALSE)
+		write.table(gsub(gsub(scan(file=simuData,what="text",sep="\n"),pattern='"',replacement='',fixed=TRUE),pattern=";",replacement=","),file="adjSimu.csv",quote=FALSE,row.names=FALSE,col.names=FALSE)
+	}else{write.table(gsub(gsub(scan(file=allData,what="text",sep="\n"),pattern='"',replacement='',fixed=TRUE),pattern=";",replacement=","),file="adjDtb.csv",quote=FALSE,row.names=FALSE,col.names=FALSE)
+		longData<-read.csv("adjDtb.csv")
+		library(tidyr)
+		bothData<-spread(longData,INDICATOR_CODE,INDICATOR_VALUE)
+		write.csv(bothData[,c(which(names(bothData)=="YEAR"),which(names(bothData)=="official.yield"|names(bothData)=="Officiall yield"|names(bothData)=="OFFICIAL_YIELD"|names(bothData)=="Official.yield"|names(bothData)=="official_yield"|names(bothData)=="Official_yield"))],"adjOffi.csv",quote=FALSE,row.names=FALSE)
+		write.csv(bothData[,c(-which(names(bothData)=="official.yield"|names(bothData)=="Officiall yield"|names(bothData)=="OFFICIAL_YIELD"|names(bothData)=="Official.yield"|names(bothData)=="official_yield"|names(bothData)=="Official_yield"))],"adjSimu.csv",quote=FALSE,row.names=FALSE)
+	}
+		eurostat<-read.csv("adjOffi.csv")
+	prev <- read.csv("adjSimu.csv")
+	#here ends the importing data issue, from here they are only structured
+
+
+	actualYield<-unique(eurostat)
+	relatedModel<-unique(prev)
+
+	#standarizing Official Yield Names...trying to, at least
+			if(any(names(actualYield)=="official.yield")){names(actualYield)[names(actualYield)=="official.yield"]<-"OFFICIAL_YIELD"}
+			if(any(names(actualYield)=="Official yield")){names(actualYield)[names(actualYield)=="Official yield"]<-"OFFICIAL_YIELD"}
+			if(any(names(actualYield)=="Official.yield")){names(actualYield)[names(actualYield)=="Official.yield"]<-"OFFICIAL_YIELD"}
+			if(any(names(actualYield)=="official_yield")){names(actualYield)[names(actualYield)=="official_yield"]<-"OFFICIAL_YIELD"}
+			if(any(names(actualYield)=="Official _yield")){names(actualYield)[names(actualYield)=="Official_yield"]<-"OFFICIAL_YIELD"}
+	#standarizing otherr variables
+	if(any(names(actualYield)=="STAT_CROP_NO")&any(names(prev)=="CROP_NO")){names(actualYield)[names(actualYield)=="STAT_CROP_NO"]<-"CROP_NO"}else{
+			if(any(names(prev)=="STAT_CROP_NO")&any(names(eurostat)=="CROP_NO")){names(prev)[names(prev)=="STAT_CROP_NO"]<-"CROP_NO"}}
+
+	if(any(names(actualYield)=="CROP_NO")&any(names(relatedModel)=="CROP_NO")& !missing(crp)){actualYield<-subset(actualYield,actualYield$CROP_NO == crp)
+		relatedModel<-subset(relatedModel,relatedModel$CROP_NO == crp)
+		}
+	if(any(names(actualYield)=="NUTS_CODE")&any(names(relatedModel)=="NUTS_CODE")& !missing(cnt)){
+		actualYield<-subset(actualYield,actualYield$NUTS_CODE == cnt)
+		relatedModel<-subset(relatedModel, relatedModel$NUTS_CODE == cnt)
+	}
+
+	yieldPrev$currentYear<- max(unique(relatedModel$YEAR))
+	if(missing(dec)){
+		currentDecade<- max(subset(relatedModel,relatedModel$YEAR==currentYear)$DECADE)
+	}else{currentDecade<-dec}
+	yieldPrev$relatedModel<-subset(relatedModel,relatedModel$DECADE == currentDecade)[,c(-which(names(prev)=="CROP_NO"),-which(names(prev)=="DECADE"),-which(names(prev)=="NUTS_CODE"))]
+}
+
+#
+#impreadSet<-list(offiData,simuData,allData,crp,cnt,dec)
+impreadSet<-list(allData="../sugar/sugar.csv",dec=15)
+
+
+batchFore<-function(impreadSet,modKind){
+do.call(importYield,impreadSet)
+yieldPrev$flatYield<-yieldPrev$actualYield
+#modSel automated choice, it have to get edited for:
+#	autoselection of proper model based on R2
+#	receive argument for standard enhanced
+dummy<-capture.output(modSel(modKind,1))
+
+#response on table
+
+return(yieldPrev$expYield)
+}
+
+#NB
+#importYield is going to get feed by a over function wich will give it a a list as argument!
